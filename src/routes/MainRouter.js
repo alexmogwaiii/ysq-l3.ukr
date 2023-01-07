a3e5f386d1867bf546aa0c9ba0620ec897701ce8
o.krainii <krainii.oleksandr@gmail.com>
2023-01-07 21:23:42 +0200

    change url in router

--- src/routes/MainRouter.js
+++ src/routes/MainRouter.js
@@ -7,8 +7,8 @@ export const MainRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<MainLayout />}>
-          <Route path='/' exact element={<Intro />} />
+        <Route path='/ysq-l3.urk' element={<MainLayout />}>
+          <Route path='/ysq-l3.urk' exact element={<Intro />} />
           <Route path='/survey' element={<div />} />
           <Route path='/results' element={<div />} />
         </Route>
