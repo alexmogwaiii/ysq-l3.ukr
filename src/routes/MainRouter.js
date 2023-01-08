d8fc8203a2ce2f485d2acb59881f75a03f2a3b85
o.krainii <krainii.oleksandr@gmail.com>
2023-01-08 02:27:05 +0200

    fix router

--- src/routes/MainRouter.js
+++ src/routes/MainRouter.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Route, HashRouter, Routes } from 'react-router-dom';
 import { Intro } from '../pages/Intro';
 import { MainLayout } from '../Layouts/MainLayout';
 
 export const MainRouter = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<MainLayout />}>
-          <Route path='/' exact element={<Intro />} />
+          <Route path='/' element={<Intro />} />
           <Route path='survey' element={<div />} />
           <Route path='results' element={<div />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
